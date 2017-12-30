14355a16a35a6b8daf2e8cc2a3d564c919b64904
LemonAtSu <at_7788546@hotmail.com>
2017-12-31 03:25:52 +0800

    add more training args

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ from cycleGAN import CycleGAN
 from collections import OrderedDict
 
 parser = argparse.ArgumentParser(description="Training parameters")
-#parser.add_argument("--dataset", default="unvoiced/Jazz_val.npy,unvoiced/Pop_val.npy", type=str, help="comma-separated dataset paths")
-parser.add_argument("--dataset", default="unvoiced/Jazz_train.npy,unvoiced/Pop_train.npy", type=str, help="comma-separated dataset paths")
+parser.add_argument("--dataset", default="unvoiced/Jazz_val.npy,unvoiced/Pop_val.npy", type=str, help="comma-separated dataset paths")
+#parser.add_argument("--dataset", default="unvoiced/Jazz_train.npy,unvoiced/Pop_train.npy", type=str, help="comma-separated dataset paths")
 #parser.add_argument("--dataset", default="unvoiced/dummyA.npy,unvoiced/dummyB.npy", type=str, help="comma-separated dataset paths")
 parser.add_argument("--output-dir", default="model_weight", type=str, help="dir for outputting log/model")
 parser.add_argument("--lr", default=0.0002, type=float, help="init learning rate")
@@ -17,6 +17,7 @@ parser.add_argument("--lamb-B", default=10., type=float, help="weight for B->A->
 parser.add_argument("--lamb-I", default=0.01, type=float, help="weight for identity loss")
 parser.add_argument("--lamb-E", default=0.0, type=float, help="weight for identity loss")
 parser.add_argument("--batch-size", default=1, type=int, help="batch size")
+parser.add_argument("--n-D", default=5, type=int, help="update freq of D")
 parser.add_argument("--pool-size", default=50, type=int, help="data pool size for discriminator")
 parser.add_argument("--gpu-ids", default="3", type=str, help="comma-separated gpu ids")
 parser.add_argument("--n-fft", default=2048, type=int, help="n fft")
@@ -24,7 +25,7 @@ parser.add_argument("--hop-length", default=512, type=int, help="n fft")
 parser.add_argument("--n-sec", default=8.16, type=float, help="n sec for each chunk")
 parser.add_argument("--sr", default=16000, type=int, help="n sec for each chunk")
 parser.add_argument("--n-epoch", default=1000, type=int, help="total training epoch")
-parser.add_argument("--log-interval", default=100, type=int, help="steps between each log")
+parser.add_argument("--log-interval", default=1, type=int, help="steps between each log")
 
 args = parser.parse_args()
 logger = Logger("logdir")
@@ -48,8 +49,6 @@ val_A = data.get_fft_npy_loader("unvoiced/Jazz_val.npy", batch_size=1)
 val_B = data.get_fft_npy_loader("unvoiced/Pop_val.npy", batch_size=1)
 
 cnt = 0
-rplA = Pool(args.pool_size)
-rplB = Pool(args.pool_size)
 
 def accumulate_report(old_report, new_report=None):
     result = OrderedDict()
@@ -69,10 +68,8 @@ for i in range(args.n_epoch):
     for j, (a, b) in enumerate(zip(train_A, train_B)):
         start = time.time()
         model.set_input({"A": a[0], "B": b[0]})
-        model.optimize()
+        model.optimize(n_D=args.n_D)
         cnt += 1
-        rplA.draw(a[0])
-        rplB.draw(b[0])
 
         if errors is None:
             errors = accumulate_report(model.report_errors())
@@ -84,8 +81,8 @@ for i in range(args.n_epoch):
 
             if cnt % (args.log_interval * 20) == 0:
                 audio = OrderedDict()
-                real_As = rplA.get_samples(2)
-                real_Bs = rplB.get_samples(2)
+                real_As = model.rplA.get_samples(2)
+                real_Bs = model.rplB.get_samples(2)
                 for k, (a, b, va, vb) in enumerate(zip(real_As, real_Bs, val_A, val_B)):
                     model.set_input({"A": a, "B": b})
                     fake_A, fake_B, recn_A, recn_B = model.generate()
